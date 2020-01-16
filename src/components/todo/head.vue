470fc4297d684572528c309ea85d5d1e4b200b9b
liuqingqing05 <liuqingqing05@meituan.com>
2020-01-16 15:18:33 +0800

    else

--- src/components/todo/head.vue
+++ src/components/todo/head.vue
@@ -1,16 +1,16 @@
 <template>
     <header class="header">
         <h1>todos</h1>
-        <input class="new-todo" autofocus autocomplete="off" placeholder="What needs to be done?" v-model="newTodo" @keyup.enter="addTodo">
+        <input class="new-todo" autofocus autocomplete="off" placeholder="What needs to be done?" v-model="state.newTodo" @keyup.enter="addTodo">
     </header>
 </template>
 <script>
 import { useState, addTodo } from "./todo";
 export default {
     setup(){
-        const {newTodo} = useState()
+        const state= useState()
         return {
-            newTodo,
+            state,
             addTodo
         }
     }
